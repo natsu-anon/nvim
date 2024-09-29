88fd3cb5054e26173dc02ebb0cb8d2b97c3e1fb8
Jon Arano <jarano93@gmail.com>
2024-09-28 22:22:21 -0700

    check lua/local/init.lua EXISTS before requiring since it's ignored

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- NOTE: run `nvim-qt -qwindowgeometry WIDTHxHEIGHT` to launch nvim-qt with whatever dims you want
 require("personal")
-require("local")
+if vim.fn.filereadable("lua/local/init.lua") == 1 then
+	require("local")
+end
 
 -- vim.cmd.colorscheme("monokai-pro-default")
 vim.cmd.colorscheme("rose-pine-main")
