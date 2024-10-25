8be27ae999865d2cbcb6cee742c30866501a3b43
Lol Lmao <lol@lmao.limo>
2024-10-24 23:20:42 -0700

    Codesnab xDDDd

---
+++ lua/personal/lazy/codesnap.lua
@@ -0,0 +1,10 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = "make",
+	opts = {
+		save_path = "~/Pictures/codesnaps/",
+		code_font_family = "Fira Sans",
+		bg_padding = 0,
+		watermark = "",
+	},
+}
